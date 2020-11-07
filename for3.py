f9696f28ea4fbf16f6f7987de467590f39c7a0c3
Rodion Balaniuc <70860169+Rxdxxn@users.noreply.github.com>
2020-11-07 22:55:11 +0200

    Add files via upload

---
+++ for3.py
@@ -0,0 +1,3 @@
+n=eval(input("introduceti un numar:"))
+for i in range(0,n+1,2):
+    print (i,end=' ')
---
+++ for4.py
@@ -0,0 +1,8 @@
+a=eval(input("introduceti primul numar:"))
+b=eval(input("introduceti al doilea numar:"))
+if a%2==0:
+    for i in range(a+1,b,2):
+        print(i,end=' ')
+else:
+    for i in range(a,b,2):
+        print(i,end=' ')
